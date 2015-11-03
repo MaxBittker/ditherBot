ef48acc062480c6f02c20f9143cc5d232bc1d42f
maxbittker <maxbittker@gmail.com>
2015-11-03 18:21:50 -0500

    asdasd

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
-// Our Twitter library
 var Twit = require('twit');
-
-// We need to include our configuration file
 var T = new Twit(require('./config.js'));
 var request = require('request');
 var fs = require("fs");
@@ -9,69 +6,60 @@ var gm = require("gm").subClass({
     imageMagick: true
 });
 
-function watchTag() {
+var stream = T.stream('statuses/filter', {
+    track: ['@DitheredBot']
+})
 
-    var stream = T.stream('statuses/filter', {
-        // track: ['art']
-        track: ['@DitheredBot']
-    })
+stream.on('tweet', function(tweet) {
+    if (tweet.entities.media == undefined)
+        return
 
-    stream.on('tweet', function(tweet) {
-        try {
-            var imgurl = tweet.entities.media[0].media_url
-            var id = imgurl.split("/")[4]
-            var outputFileName = 'output/' + id
-            var nameID = tweet.id_str;
-            var name = tweet.user.screen_name;
+    var imgurl = tweet.entities.media[0].media_url
+    var id = imgurl.split("/")[4]
 
-            var stream = request(imgurl)
-            gm(stream, outputFileName).normalize().monochrome().write(outputFileName, function(err) {
+    var name = tweet.user.screen_name;
+    var nameID = tweet.id_str;
+    
+    var outputFileName = 'output/' + id
 
-                if (!err) {
-                    console.log('wrote: ' + id);
-                    // console.log(name)
-                    tweetImage(outputFileName, name, nameID)
-                }
+    var stream = request(imgurl)
 
-            });
-
-        } catch (e) {
-            // console.log(e)
+    gm(stream, outputFileName).normalize().monochrome().write(outputFileName, function(err) {
+        if (!err) {
+            console.log('wrote: ' + id);
+            tweetImage(outputFileName, name, nameID)
         }
-    })
 
+    });
 
-}
+})
+
 
 function tweetImage(path, name, nameID) {
 
     fs.readFile(path, {
             encoding: 'base64'
         },
-        
+
         function(err, buf) {
             var b64content = buf
 
             T.post('media/upload', {
                 media_data: b64content
             }, function(err, data, response) {
-                // now we can reference the media and post a tweet (media will attach to the tweet)
+
                 var mediaIdStr = data.media_id_string
                 var params = {
-                    // status: '#dithered',
+                    status: 'dithered!' + ' @' + name,
                     media_ids: [mediaIdStr],
-                    in_reply_to_status_id: nameID,
-                    status: 'dithered!' + ' @' + name
+                    in_reply_to_status_id: nameID
                 }
 
                 T.post('statuses/update', params, function(err, data, response) {
-                    // console.log(data)
                     console.log('tweeted: ' + params.status)
                 })
+                
                 fs.unlink(path, function() {})
             })
         });
 }
-
-
-watchTag();
